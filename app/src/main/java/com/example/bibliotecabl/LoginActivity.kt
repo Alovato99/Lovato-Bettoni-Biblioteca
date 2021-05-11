f629f9a83f0fbfc7b354bf1fa4794a3e9fc86faf
diego <diego.bett@hotmail.it>
2021-05-11 17:57:38 +0200

    Init ver 0.0.2 field checker implemented

--- app/src/main/java/com/example/bibliotecabl/LoginActivity.kt
+++ app/src/main/java/com/example/bibliotecabl/LoginActivity.kt
@@ -2,11 +2,59 @@ package com.example.bibliotecabl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+//import kotlinx.android.synthetic.main.activity_login.*
+
+// ### TEST IMPORT ###
+import kotlinx.android.synthetic.main.activity_signup.*
+//import kotlinx.android.synthetic.main.activity_test.*
 
 class LoginActivity : AppCompatActivity() {
+
+    // Public field checker instance
+    val checker = fieldChecker()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         //start application commit test
+    }
+
+
+    fun checkLogin(v: View?)
+    {
+        // ### CHECKING LOGIN CREDENTIALS ###
+        val email: String = emailEditText.getText().toString()
+        if(!checker.isValidEmail(email))
+        {
+            emailEditText.setError(getString(R.string.invalidEmail))
+        }
+
+
+        val pwd: String = passwordEditText.getText().toString()
+        if(!checker.isValidPassword(pwd))
+        {
+            passwordEditText.setError(getString(R.string.invalidPwd))
+        }
+
+        val pwdRepeat: String = repeatPasswordEditText.getText().toString()
+        if(!checker.isEqualPassword(pwd, pwdRepeat))
+        {
+            repeatPasswordEditText.setError(getString(R.string.pwdNotMatch))
+        }
+
+
+        val name: String = nameEditText.getText().toString()
+        if(checker.isInvalidNameOrSurname(name))
+        {
+            nameEditText.setError(getString(R.string.invalidName))
+        }
+        val surnamename: String = nameEditText.getText().toString()
+        if(checker.isInvalidNameOrSurname(surnamename))
+        {
+            surnameEditText.setError(getString(R.string.invalidSurname))
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/bibliotecabl/fieldChecker.kt
@@ -0,0 +1,37 @@
+package com.example.bibliotecabl
+
+import java.util.regex.Pattern
+
+class fieldChecker {
+
+    private val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+    private val NAME_PATTERN = (".*\\d.*")
+
+    fun isValidEmail(email: String): Boolean
+    {
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
+    fun isValidPassword(pwd: String) : Boolean
+    {
+        return (pwd !=null && pwd.length >=4)
+    }
+
+    fun isEqualPassword(pwd1: String, pwd2: String):Boolean
+    {
+        return pwd1.equals(pwd2)
+    }
+
+    fun isInvalidNameOrSurname(name: String): Boolean
+    {
+        if (name.length <4)
+            return true
+        val pattern = Pattern.compile(NAME_PATTERN)
+        val matcher = pattern.matcher(name)
+        return matcher.matches()
+
+    }
+
+}
