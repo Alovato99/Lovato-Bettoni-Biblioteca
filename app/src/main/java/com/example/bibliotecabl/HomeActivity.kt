fae08de2f9b89878f19e0e7b138a7c90374350c3
Alessio <alessio.lovato.99@mail.com>
2021-05-17 11:35:40 +0200

    Version 0.0.3.4.6

--- app/src/main/java/com/example/bibliotecabl/HomeActivity.kt
+++ app/src/main/java/com/example/bibliotecabl/HomeActivity.kt
@@ -1,11 +1,27 @@
 package com.example.bibliotecabl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null)
+        val currentUser = auth.currentUser
+        if(currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
